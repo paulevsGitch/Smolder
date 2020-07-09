36df59bd7d104e8c27ce7b7c7f105e00e3bc5f7c
paulevsGitch <paulevs@yandex.ru>
2020-07-10 00:52:39 +0300

    Sub-biomes fixes

--- src/main/java/com/terraformers/smolder/generator/BiomeChunk.java
+++ src/main/java/com/terraformers/smolder/generator/BiomeChunk.java
@@ -35,46 +35,6 @@ public class BiomeChunk
 			indexes[i] = indexes[i2];
 			indexes[i2] = a;
 		}
-
-		for (int i: indexes)
-		{
-			if (biomes[i] == null)
-			{
-				SmolderBiome biome = SmolderBiomeRegistry.getRandomBiome(random);
-				float size = biome.getSize() * SCALE;
-				float r2 = size * size;
-				int x = i / WIDTH;
-				int z = i & MASK_W;
-				int x1 = (int) (x - size);
-				int z1 = (int) (z - size);
-				int x2 = (int) Math.ceil(x + size);
-				int z2 = (int) Math.ceil(z + size);
-				for (int bx = x1; bx <= x2; bx++)
-				{
-					if (bx >= 0 && bx < WIDTH)
-					{
-						int bx2 = (bx - x);
-						bx2 *= bx2;
-						for (int bz = z1; bz <= z2; bz++)
-						{
-							if (bz >= 0 && bz < WIDTH)
-							{
-								int bz2 = (bz - z);
-								bz2 *= bz2;
-								if (bx2 + bz2 <= r2)
-								{
-									int i2 = getIndex(bx, bz);
-									if (biomes[i2] == null)
-									{
-										biomes[i2] = SmolderBiomeRegistry.getSubBiome(biome, random);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
 		
 		SUBLIST.clear();
 		List<Integer> list = null;
@@ -120,7 +80,7 @@ public class BiomeChunk
 									int i2 = getIndex(bx, bz);
 									if (biomes[i2] == null)
 									{
-										biomes[i2] = biome;//SmolderBiomeRegistry.getSubBiome(biome, random);
+										biomes[i2] = biome;
 										if (hasSub)
 										{
 											list.add(i2);
@@ -177,7 +137,7 @@ public class BiomeChunk
 									if (bx2 + bz2 <= r2)
 									{
 										int i2 = getIndex(bx, bz);
-										if (biomes[i2] == parent && (!hasSub || list2.contains(i2)))
+										if (biomes[i2] == parent && (!hasSub || !list2.contains(i2)))
 										{
 											biomes[i2] = biome;
 											if (hasSub && biome != parent)
