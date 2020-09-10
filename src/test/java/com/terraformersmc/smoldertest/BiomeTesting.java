a7225de6c60ed4d7f208a45370a4c429e5aba2f0
paulevsGitch <paulevs@yandex.ru>
2020-09-11 00:17:52 +0300

    Added testmod

--- src/test/java/com/terraformersmc/smoldertest/BiomeTesting.java
+++ src/test/java/com/terraformersmc/smoldertest/BiomeTesting.java
@@ -1,3 +1,5 @@
+package com.terraformersmc.smoldertest;
+
 import com.terraformersmc.smolder.api.SmolderBiomeRegistry;
 import com.terraformersmc.smolder.biome.BiomeDefinition;
 
@@ -7,6 +9,7 @@ import net.minecraft.util.Identifier;
 public class BiomeTesting implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		SmolderBiomeRegistry.registerBiome(new Identifier("smolder", "test_biome"), new BiomeDefinition().setFogColor(0, 255, 0));
+		System.out.println("Test smolder biomes");
+		SmolderBiomeRegistry.registerBiome(new Identifier("smoldertest", "test_biome"), new BiomeDefinition().setFogColor(0, 255, 0));
 	}
 }
