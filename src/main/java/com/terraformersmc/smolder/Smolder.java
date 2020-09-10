c31ab782939cdfa3f2d13b7ce44a71480e80d6fc
paulevsGitch <paulevs@yandex.ru>
2020-09-10 18:58:38 +0300

    1.16.2 prototype

--- src/main/java/com/terraformersmc/smolder/Smolder.java
+++ src/main/java/com/terraformersmc/smolder/Smolder.java
@@ -1,12 +1,12 @@
 package com.terraformersmc.smolder;
 
 import com.terraformersmc.smolder.api.SmolderBiomeRegistry;
-import com.terraformersmc.smolder.biome.SmolderBiome;
 import com.terraformersmc.smolder.world.SmolderBiomeSource;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome.Category;
 
@@ -18,7 +18,9 @@ public class Smolder implements ModInitializer {
 	public void onInitialize() {
 		SmolderBiomeRegistry.init();
 		Registry.register(Registry.BIOME_SOURCE, new Identifier(MOD_ID, "smolder_biome_source"), SmolderBiomeSource.CODEC);
-		RegistryEntryAddedCallback.event(Registry.BIOME).register((i, id, biome) -> SmolderBiomeRegistry.registerBiome(biome));
+		RegistryEntryAddedCallback.event(BuiltinRegistries.BIOME).register((i, id, biome) -> {
+			if (biome.getCategory() == Category.NETHER) SmolderBiomeRegistry.registerBiome(biome);
+		});
 		registerExistingBiomes();
 	}
 	
@@ -26,8 +28,8 @@ public class Smolder implements ModInitializer {
 	 * Register existing biomes on library startup. Must be called once.
 	 */
 	private void registerExistingBiomes() {
-		Registry.BIOME.forEach((biome) -> {
-			if (biome.getCategory() == Category.NETHER && !(biome instanceof SmolderBiome)) {
+		BuiltinRegistries.BIOME.forEach((biome) -> {
+			if (biome.getCategory() == Category.NETHER && !(SmolderBiomeRegistry.getFromBiome(biome) != null)) {
 				SmolderBiomeRegistry.registerBiome(biome);
 			}
 		});
